2b1eceeb88bd76dc63dc279f813e59ad072686a2
Neriya Cohen <ee10293847@gmail.com>
2024-02-19 13:54:36 +0200

    Cleanup

--- src/jsx/html/_auto_html.py
+++ src/jsx/html/_auto_html.py
@@ -2,8 +2,6 @@ import pathlib
 
 HERE = pathlib.Path(__file__).parent
 
-out_string = ""
-
 with open(HERE / "__init__.py", "w") as f:
     for file in HERE.iterdir():
         if file.name.startswith("_"):
