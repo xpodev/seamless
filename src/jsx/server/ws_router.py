1f953a2cfa6aad82a3ea43c2706cf28ec7ed8af7
Neriya Cohen <ee10293847@gmail.com>
2024-04-09 20:28:56 +0300

    Fix handlers to use acknowledgement

--- src/jsx/server/ws_router.py
+++ src/jsx/server/ws_router.py
@@ -1,7 +1,6 @@
 from enum import Enum
 from .components import COMPONENTS_REPOSITORY
 from jsx.renderer import render_json
-from jsx.server.database import DB
 
 
 class WSRouterCommands(str, Enum):
@@ -10,7 +9,7 @@ class WSRouterCommands(str, Enum):
 
 def get_component(socket_id: str, name: str, props={}):
     cls = COMPONENTS_REPOSITORY.get_component(name)
-    return "component", render_json(cls(**props))
+    return render_json(cls(**props))
 
 
 ws_router = {
