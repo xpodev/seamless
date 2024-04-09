29b6177be5ca31ef5a02ad0909b61a8f17a2449e
Neriya Cohen <ee10293847@gmail.com>
2024-04-09 19:31:36 +0300

    Add event object transformer in options

--- node/packages/core/src/types.ts
+++ node/packages/core/src/types.ts
@@ -2,4 +2,13 @@ import type { SocketOptions, ManagerOptions } from 'socket.io-client';
 
 export interface SlarfOptions {
     socketOptions?: Partial<ManagerOptions & SocketOptions>;
+    /**
+     * A function that serializes the event object before sending it to the server
+     * The default behavior is to return the event object as is
+     * 
+     * @param originalEvent The original event object
+     * @param outEvent The event object that is constructed by `serializeEventObject`
+     * @returns The object that will be sent to the server
+     */
+    eventObjectTransformer?: (originalEvent: Event, outEvent: any) => any;
 }
