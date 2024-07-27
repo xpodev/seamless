5f0c23b3eccd31e874011cb769ebc60a0b070b71
Neriya Cohen <ee10293847@gmail.com>
2024-07-28 02:17:32 +0300

    Fix #35

--- seamless/rendering/transformers/events_transformer.py
+++ seamless/rendering/transformers/events_transformer.py
@@ -7,15 +7,23 @@ def events_transformer():
     def matcher(key: str, value):
         return key.startswith("on_") and callable(value)
 
-    def event_transformer(key: str, value, element_props):
+    def event_transformer(key: str, value, props):
         if not callable(value):
-            element_props[key] = value
+            props[key] = value
             return
         
         event_name = key.removeprefix("on_")
         action = DB.add_event(value)
-        element_props[f"{EVENTS_NAMESPACE}:{event_name}"] = action.id
-        element_props[ELEMENT_ATTR] = True
+        props["seamless:init"] = props.get("seamless:init", "") + \
+        f"""this.addEventListener('{event_name}', (event) => {{
+            const outEvent = seamless.instance.eventObjectTransformer(
+              event, 
+              seamless.instance.serializeEventObject(event)
+            );
+          seamless.instance.socket.emit("event", "{action.id}", outEvent);
+        }});"""
+
+        props[ELEMENT_ATTR] = True
 
     return matcher, event_transformer
 
