c075911a331b8de880c32b8ece5e93a1dfb7885a
Neriya Cohen <ee10293847@gmail.com>
2024-07-27 22:45:30 +0300

    Update dev.ps1 in node

--- node/dev.ps1
+++ node/dev.ps1
@@ -1,3 +1,4 @@
 yarn build:browser
 Copy-Item -Path ./packages/core/umd/index.js -Destination ../tests/server/static/main.js
 Add-Content -Path ../tests/server/static/main.js -Value "const s = new Seamless.default();"
+Copy-Item -Path ../tests/server/static/main.js -Destination ../examples/simple/static/main.js
