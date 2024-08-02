e9dce8bf2be4cf105bbc3e1b8a327d2780c4440d
Binyamin Y Cohen <42520501+binyamin555@users.noreply.github.com>
2024-08-02 15:49:07 +0300

    Fix typing in empty.py

--- seamless/extra/empty.py
+++ seamless/extra/empty.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 from seamless.element import Element
 from seamless.internal import SEAMLESS_ELEMENT_ATTRIBUTE
 
@@ -8,6 +8,6 @@ if TYPE_CHECKING:
 class Empty(Element):
     tag_name = "seamless:empty"
 
-    def __init__(self, *args: "ChildrenType", **props: dict):
+    def __init__(self, *args: "ChildrenType", **props: Any):
         props[SEAMLESS_ELEMENT_ATTRIBUTE] = True
         super().__init__(*args, **props)
