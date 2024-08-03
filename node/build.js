6bdf4694f72062fd4f74b09d08e23c6021913610
Neriya Cohen <ee10293847@gmail.com>
2024-08-03 23:11:28 +0300

    Fix #36

---
+++ node/build.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const nextVersion = process.argv[2];
+
+const packagesDir = path.join(__dirname, 'packages');
+const packages = fs.readdirSync(packagesDir);
+packages.forEach((pkg) => {
+  const pkgPath = path.join(packagesDir, pkg);
+  if (!fs.lstatSync(pkgPath).isDirectory()) {
+    return;
+  }
+  const pkgJsonPath = path.join(pkgPath, 'package.json');
+  const pkgJson = require(pkgJsonPath);
+  pkgJson.version = nextVersion;
+  fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+  execSync(`cd ${pkgPath} && npm run build && npm publish --access public`);
+});
