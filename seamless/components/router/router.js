7d4d313af2689e958a3c7bff24a5c84371362c8e
Neriya Cohen <ee10293847@gmail.com>
2024-08-14 20:39:15 +0300

    Add loading component in router

--- seamless/components/router/router.js
+++ seamless/components/router/router.js
@@ -1,9 +1,23 @@
-const loadComponent = async function (name) {
-  return seamless.instance.toDOMElement(await seamless.instance.getComponent(name, {}));
-}
-
+const loadingComponentName = this.getAttribute("loading");
 const parent = this.parentElement;
 const PageStateChange = new Event("pageLocationChange");
+let loadingComponent = null;
+
+if (loadingComponentName) {
+  seamless.instance.getComponent(loadingComponentName, {}).then((component) => {
+    loadingComponent = seamless.instance.toDOMElement(component);
+  });
+}
+
+const clearParent = () => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+const loadComponent = async (name) => {
+  return seamless.instance.toDOMElement(await seamless.instance.getComponent(name, {}));
+}
 
 window.addEventListener("pageLocationChange", () => {
   const path = window.location.pathname;
@@ -15,11 +29,13 @@ window.addEventListener("pageLocationChange", () => {
     return;
   }
 
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+  clearParent();
+  if (loadingComponent) {
+    parent.appendChild(loadingComponent);
   }
 
   loadComponent(page.name).then((component) => {
+    clearParent();
     parent.appendChild(component);
   });
 });
