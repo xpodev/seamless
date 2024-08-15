e135d13dce0ec4e6480889323108e6972080edc9
Neriya Cohen <42520786+neriyaco@users.noreply.github.com>
2024-08-15 18:50:32 +0300

    Fix example base page

--- examples/simple/pages/base.py
+++ examples/simple/pages/base.py
@@ -9,7 +9,7 @@ class BasePage(Page):
             rel="stylesheet",
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
         )
-        yield Script(src="/static/main.js", defer=True)
+        yield Script(src="https://cdn.jsdelivr.net/npm/@python-seamless/core@0.8.6/umd/seamless.init.js", defer=True)
         yield Style(
             "html, body { height: 100%; }" +
             CSS.to_css_string(minified=True)
