1b8a9056ab1e3b3b39b0769346537d3fbcc60aef
Neriya Cohen <ee10293847@gmail.com>
2024-08-18 15:45:01 +0300

    Use seamless version in example

--- examples/simple/pages/base.py
+++ examples/simple/pages/base.py
@@ -1,4 +1,4 @@
-from seamless import Link, Script, Style
+from seamless import Link, Script, Style, __version__
 from seamless.components import Page
 from seamless.styling import CSS
 
@@ -9,7 +9,7 @@ class BasePage(Page):
             rel="stylesheet",
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
         )
-        yield Script(src="https://cdn.jsdelivr.net/npm/@python-seamless/core@0.8.6/umd/seamless.init.js", defer=True)
+        yield Script(src=f"https://cdn.jsdelivr.net/npm/@python-seamless/core@{__version__}/umd/seamless.init.js", defer=True)
         yield Style(
             "html, body { height: 100%; }" +
             CSS.to_css_string(minified=True)
