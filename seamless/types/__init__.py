47956f5bbf538d1c0d4a53ca50451abb289e5f62
Neriya Cohen <ee10293847@gmail.com>
2024-08-20 01:02:23 +0300

    Fix typing

--- seamless/types/__init__.py
+++ seamless/types/__init__.py
@@ -1,13 +1,11 @@
-from typing import Collection, TypeAlias, TYPE_CHECKING, Union
+from typing import Collection, TypeAlias, TypeVar
 
-if TYPE_CHECKING:
-    from ..core import Component
-    from ..element import Element
-
+from ..core import Component
+from ..element import Element
 
 Primitive: TypeAlias = str | int | float | bool | None
-Renderable: TypeAlias = Union["Element", "Component"]
+Renderable: TypeAlias = Element | Component
 
 ChildType: TypeAlias = Renderable | Primitive
-ChildrenType: TypeAlias = Collection[ChildType]
 RenderResult: TypeAlias = Renderable | Primitive
+ChildrenType: TypeAlias = Collection[ChildType]
