26ec3d861381eab57e3b8a78f4bf1c6814351073
Neriya Cohen <ee10293847@gmail.com>
2024-08-26 18:49:06 +0300

    Fix dash transformer

--- seamless/extra/transformers/dash_transformer.py
+++ seamless/extra/transformers/dash_transformer.py
@@ -3,7 +3,7 @@ def dash_transformer():
         return "_" in key and (isinstance(value, (str, int, float)) or value is None)
     
     def transformer(key: str, value, element):
-        dash_key = key.replace("-", "_")
+        dash_key = key.replace("_", "-")
         element.props[dash_key] = value
         del element.props[key]
 
