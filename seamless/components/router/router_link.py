36097bb1042d040bf3c86639220e95c88cb92e7e
Neriya Cohen <ee10293847@gmail.com>
2024-08-26 19:36:18 +0300

    Change router link property name

--- seamless/components/router/router_link.py
+++ seamless/components/router/router_link.py
@@ -2,11 +2,11 @@ from ... import Component, A, JS
 
 
 class RouterLink(Component):
-    def __init__(self, *, to, **kwargs):
+    def __init__(self, *, to: str, **anchor_props):
         self.to = to
-        self.kwargs = kwargs
+        self.anchor_props = anchor_props
 
     def render(self):
         return A(
-            href=self.to, on_click=JS(f"event.preventDefault(); return seamless.navigateTo('{self.to}')"), **self.kwargs
+            href=self.to, on_click=JS(f"event.preventDefault(); return seamless.navigateTo('{self.to}')"), **self.anchor_props
         )(*self.children)
