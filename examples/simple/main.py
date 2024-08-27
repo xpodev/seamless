39672bdfeb81cfffab34fe999e0c75eb641dc528
Neriya Cohen <ee10293847@gmail.com>
2024-08-27 19:14:00 +0300

    Fix examples runners

--- examples/simple/main.py
+++ examples/simple/main.py
@@ -25,4 +25,4 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- examples/spa/main.py
+++ examples/spa/main.py
@@ -1,16 +1,23 @@
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
-from seamless import render
+from seamless import render, Script
 from seamless.middlewares import ASGIMiddleware
+from seamless.rendering.transformers import post_render_transformer
 
 from components.app import App
+from seamless.rendering.tree.nodes.context_node import ContextNode
 
 HERE = Path(__file__).parent
 
 app = FastAPI()
 app.add_middleware(ASGIMiddleware)
 
+@post_render_transformer()
+def script_head(root: ContextNode):
+    head = root.get_by_tag("head")
+    if head:
+        head.append_child(Script(src="/static/script.js"))
 
 @app.get("/static/{file_path:path}")
 def read_static(file_path: str):
@@ -25,4 +32,4 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
