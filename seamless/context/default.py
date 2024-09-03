20f3f3dea3338a84f00317aba88fa081b7560c55
Neriya Cohen <ee10293847@gmail.com>
2024-09-04 02:20:32 +0300

    Remove pydom transformers from default

--- seamless/context/default.py
+++ seamless/context/default.py
@@ -5,16 +5,8 @@ from .context import Context
 from ..extra.components import ComponentsFeature
 from ..extra.events import EventsFeature
 from ..extra.state import StateFeature
-
-from ..extra.transformers.class_transformer import class_transformer
-from ..extra.transformers.dash_transformer import dash_transformer
 from ..extra.transformers.js_transformer import init_transformer, js_transformer
-from ..extra.transformers.html_events_transformer import html_events_transformer
-from ..extra.transformers.simple_transformer import simple_transformer
-from ..extra.transformers.style_transformer import style_transformer
-
-if TYPE_CHECKING:
-    from . import Context
+from ..extra.transports.socketio.transport import SocketIOTransport
 
 
 def add_standard_features(ctx: Context):
