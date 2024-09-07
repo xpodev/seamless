ca3dde99a9f31ffb36d0e388800ece02d1981a4d
Neriya Cohen <ee10293847@gmail.com>
2024-09-08 02:10:29 +0300

    Update examples

--- examples/spa/pages/base.py
+++ examples/spa/pages/base.py
@@ -9,7 +9,7 @@ class BasePage(Page):
             rel="stylesheet",
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
         )
-        yield Script(src=f"/static/seamless.init.js", defer=True)
+        yield Script(src=f"https://cdn.jsdelivr.net/npm/python-seamless/umd/seamless.init.js", defer=True)
         yield Style(
             "html, body { height: 100%; }" +
             CSS.to_css_string(minified=True)
